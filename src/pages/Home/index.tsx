22d41248a906396ee91fbb0d3958cfaa9d0cabd5
Bzzard <sirius.a015@gmail.com>
2023-04-27 16:06:38 +0200

    scroll smooth

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import HeroImg from "../../assets/images/new/back.png";
-import { useEffect, useState, useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
-import { ScrollType } from "../../types";
 import Main from "./components/Main";
 import Batman from "./components/Batman";
 import TokenDetails from "./components/TokenDetails";
@@ -17,35 +15,7 @@ const StyledPage = styled.div`
 `;
 
 const Home = () => {
-  const [scrollType, setScrollType] = useState(ScrollType.Initial);
   const isDesktop = useMediaQuery({ query: "(min-width: 1650px)" });
-  const prevScrollY = useRef(0);
-  const [goingUp, setGoingUp] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (prevScrollY.current < currentScrollY && goingUp) {
-        setGoingUp(false);
-      }
-      if (prevScrollY.current > currentScrollY && !goingUp) {
-        setGoingUp(true);
-      }
-      prevScrollY.current = currentScrollY;
-      let dt = document.body.scrollHeight - window.innerHeight;
-      if (currentScrollY < dt / 3) {
-        setScrollType(ScrollType.Initial);
-      } else if (currentScrollY < (2 * dt) / 3) {
-        setScrollType(ScrollType.Middle);
-      } else {
-        setScrollType(ScrollType.End);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [goingUp]);
 
   return (
     <StyledPage
